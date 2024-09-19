5b426c5d93936e3819dbf49c2c69dca9772dddca
rozzy <rosekaremeri@gmail.com>
2024-09-19 19:49:39 +0300

    off

--- contracts/GUZErc20Token.sol
+++ contracts/GUZErc20Token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GUZToken is ERC20 {
     constructor() ERC20("GUZToken", "GUZ") {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals()))); // 1 million GUZ tokens minted to the deployer
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals()))); 
     }
 }
 
--- contracts/W3BErc20Token.sol
+++ contracts/W3BErc20Token.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract W3BToken is ERC20 {
     constructor() ERC20("W3BToken", "W3B") {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals()))); // 1 million W3B tokens minted to the deployer
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals()))); 
     }
 }
